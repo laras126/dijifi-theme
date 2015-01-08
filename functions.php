6819110e41b3fa63642ecb6ae1e6aed4ec1a4b25
laras126 <laras126@gmail.com>
2015-01-08 10:54:19 -0500

    potential yoast/custom fields solution

--- functions.php
+++ functions.php
@@ -159,5 +159,32 @@
 	// Include other files from lib/
 
 	require('lib/widgets.php'); // Widgets
+
+
+	// Make custom fields work with Yoast SEO (only impacts the light, but helpful!)
+	// https://imperativeideas.com/making-custom-fields-work-yoast-wordpress-seo/
+
+	if ( is_admin() ) { // check to make sure we aren't on the front end
+		add_filter('wpseo_pre_analysis_post_content', 'add_custom_to_yoast');
+
+		function add_custom_to_yoast( $content ) {
+			global $post;
+			$pid = $post->ID;
+			
+			$custom = get_post_custom($pid);
+			unset($custom['_yoast_wpseo_focuskw']); // Don't count the keyword in the Yoast field!
+
+			foreach( $custom as $key => $value ) {
+				if( substr( $key, 0, 1 ) != '_' && substr( $value[0], -1) != '}' && !is_array($value[0]) && !empty($value[0])) {
+				  $custom_content .= $value[0] . ' ';
+				}
+			}
+
+			$content = $content . ' ' . $custom_content;
+			return $content;
+
+			remove_filter('wpseo_pre_analysis_post_content', 'add_custom_to_yoast'); // don't let WP execute this twice
+		}
+	}
 	
 ?>
