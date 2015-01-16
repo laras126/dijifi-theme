63ea893bd10c1f2234167e880796401b4c3769f4
laras126 <laras126@gmail.com>
2015-01-16 14:37:55 -0500

    add analytics function

--- functions.php
+++ functions.php
@@ -151,6 +151,14 @@
 	}
 	add_filter( 'acf/fields/wysiwyg/toolbars' , 'dfi_acf_wysiwyg_toolbar'  );
 
+	
+
+	// ACF Options Page
+	
+	if( function_exists('acf_add_options_page') ) {
+		acf_add_options_page('Theme Settings');
+	}
+
 
 
 	// Include other files from lib/
@@ -164,7 +172,7 @@
 	// https://imperativeideas.com/making-custom-fields-work-yoast-wordpress-seo/
 
 	if ( is_admin() ) { // check to make sure we aren't on the front end
-		add_filter('wpseo_pre_analysis_post_content', 'add_custom_to_yoast');
+		add_filter('wpseo_pre_analysis_post_content', 'dfi_add_custom_to_yoast');
 
 		function add_custom_to_yoast( $content ) {
 			global $post;
@@ -182,7 +190,7 @@
 			$content = $content . ' ' . $custom_content;
 			return $content;
 
-			remove_filter('wpseo_pre_analysis_post_content', 'add_custom_to_yoast'); // don't let WP execute this twice
+			remove_filter('wpseo_pre_analysis_post_content', 'dfi_add_custom_to_yoast'); // don't let WP execute this twice
 		}
 	}
 
@@ -191,23 +199,39 @@
 	// Load Gravity Forms JS in the footer...really? Sheesh.
 	// https://bjornjohansen.no/load-gravity-forms-js-in-footer
 
-	function wrap_gform_cdata_open( $content = '' ) {
+	function dfi_wrap_gform_cdata_open( $content = '' ) {
 		$content = 'document.addEventListener( "DOMContentLoaded", function() { ';
 		return $content;
 	}
-	add_filter( 'gform_cdata_open', 'wrap_gform_cdata_open' );
+	add_filter( 'gform_cdata_open', 'dfi_wrap_gform_cdata_open' );
 
-	function wrap_gform_cdata_close( $content = '' ) {
+	function dfi_wrap_gform_cdata_close( $content = '' ) {
 		$content = ' }, false );';
 		return $content;
 	}
-	add_filter( 'gform_cdata_close', 'wrap_gform_cdata_close' );
+	add_filter( 'gform_cdata_close', 'dfi_wrap_gform_cdata_close' );
 
 
 
-	// ACF Options Page
-	if( function_exists('acf_add_options_page') ) {
-		acf_add_options_page('Theme Settings');
+	// Google Analytics snippet from HTML5 Boilerplate
+ 
+	define('GOOGLE_ANALYTICS_ID', 'UA-2286269-1');
+	
+	function dfi_google_analytics() { ?>
+		<script>
+  			<?php if (WP_ENV === 'production') : ?>
+    			(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;e=o.createElement(i);r=o.getElementsByTagName(i)[0];e.src='//www.google-analytics.com/analytics.js';r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  			<?php else : ?>
+    			function ga() {
+      				console.log('GoogleAnalytics: ' + [].slice.call(arguments));
+    			}
+  			<?php endif; ?>
+  			ga('create','<?php echo GOOGLE_ANALYTICS_ID; ?>','auto');ga('send','pageview');
+		</script> <?php 
+	}
+
+	if (GOOGLE_ANALYTICS_ID && (WP_ENV !== 'production' || !current_user_can('manage_options'))) {
+	  add_action('wp_footer', 'dfi_google_analytics', 20);
 	}
 	
 ?>
