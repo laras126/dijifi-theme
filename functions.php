0c22a71d8f553d24e81e5096e4d62dd3695a809a
laras126 <laras126@gmail.com>
2015-01-15 21:58:10 -0500

    change to min

--- functions.php
+++ functions.php
@@ -89,7 +89,7 @@
 		}
 
 		// Add our JS
-		wp_enqueue_script( 'js', get_template_directory_uri() . '/assets/js/build/scripts.js', array('jquery'), '1.0.0', true );
+		wp_enqueue_script( 'js', get_template_directory_uri() . '/assets/js/build/scripts.min.js', array('jquery'), '1.2.0', true );
 	}
 	add_action( 'wp_enqueue_scripts', 'dfi_scripts' );
 
